2dd47b3d04359064bade29e8e106759562319135
Dasha Niemkevych <daria.niemkevych@gmail.com>
2019-06-26 19:04:55 +0300

    Изменена формула для импульсов

--- sketches/impulse.js
+++ sketches/impulse.js
@@ -74,14 +74,35 @@ class Ball {
             if (dist.mag() < 2*this.radius) {
                 dist.setMag(2*this.radius);
                 this.pos = p5.Vector.add(dist, elem.pos);
+                /*let angle = this.vel.angleBetween(elem.vel);
+                let angle1 = elem.vel.angleBetween(dist);
+                dist.mult(-1);
+                let angle2 = this.vel.angleBetween(dist);
+                let a = sin(angle1)*(2*this.radius - dist.mag())/sin(angle2);
+                let b = sin(angle2)*(2*this.radius - dist.mag())/sin(angle2);        TODO
+                let change1 = this.vel.copy();
+                let change2 = elem.vel.copy();
+                change1.setMag(a);
+                change2.setMag(b);
+                this.pos.sub(change1);
+                elem.pos.sub(change2);
+                dist.setMag(2*this.radius);*/
             }
             if (dist.mag() == 2*this.radius) {
-                let newVel1 = p5.Vector.sub(elem.vel, this.vel); //to do
-                newVel1.mult(2*this.mass/(this.mass + elem.mass));
-                let newVel2 = p5.Vector.sub(this.vel, elem.vel);
-                newVel2.mult(2*elem.mass/(this.mass + elem.mass));
-                this.vel.add(newVel1);
-                elem.vel.add(newVel2);
+                let newVel1 = createVector();
+                let newVel2 = createVector();
+                let direct = p5.Vector.sub(this.pos, elem.pos);
+                let angle = -direct.heading();
+                this.vel.rotate(angle);
+                elem.vel.rotate(angle);
+                newVel1.y = this.vel.y;
+                newVel2.y = elem.vel.y;
+                newVel1.x = ((this.mass - elem.mass)*this.vel.x + 2*elem.mass*elem.vel.x)/(this.mass + elem.mass);
+                newVel2.x = ((elem.mass - this.mass)*elem.vel.x + 2*this.mass*this.vel.x)/(this.mass + elem.mass);
+                newVel1.rotate(-angle);
+                newVel2.rotate(-angle);
+                this.vel = newVel1;
+                elem.vel = newVel2;
             }
         }
     }

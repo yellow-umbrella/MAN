bfc7d4c1801d1583d7d3569827835e7c25d7bbaf
Dasha Niemkevych <daria.niemkevych@gmail.com>
2019-07-18 22:13:07 +0300

    Изменена пружинка

--- sketches/springs.js
+++ sketches/springs.js
@@ -3,8 +3,7 @@ module.exports = new p5((P) => { with (P) {
 
 let gravity;
 let spring, ball;
-let coeff, mass;
-let running;
+let running, steps = 10;
 let coeffS, massS;
 let resetB, runB;
 
@@ -18,22 +17,23 @@ P.setup = function() {
     runB = createRunB(P, run);
 
     gravity = createVector(0, 1);
-    ball = new Ball();
-    spring = new Spring(ball);
     running = true;
-
-    massS = createSlider(1, 10, 1, 1);
-    coeffS = createSlider(0.025, 0.5, 0.025, 0.025);
+    
+    massS = createSlider(0.5, 2, 1.25, 0.25);
+    coeffS = createSlider(0.025, 0.25, 0.125, 0.025);
     massS.position(0, 20);
     coeffS.position(0, 20);
-
+    
     let labelMass = createElement('label', 'Маса тіла:');
     labelMass.elt.appendChild(massS.elt);
     labelMass.position(5, 50);
-
+    
     let labelCoeff = createElement('label', 'Жорсткість пружини:');
     labelCoeff.elt.appendChild(coeffS.elt);
     labelCoeff.position(5, 100);
+
+    ball = new Ball();
+    spring = new Spring(ball);
 
     loadFont('./fonts/Roundedmplus1c.ttf', font => textFont(font));
 }
@@ -41,33 +41,39 @@ P.setup = function() {
 P.draw = function() {
     background('#1b4b34');
 
-    coeff = coeffS.value();
-    mass = massS.value();
+    if (mouseIsPressed) {
+        if (mouseY > 0 && mouseX > 0 && mouseX < width && mouseY < height) {
+            if (mouseY < spring.pos.y + max(spring.links, spring.len/3)) {
+                ball.pos.y = spring.pos.y + max(spring.links, spring.len/3);
+            } else if (mouseY - spring.pos.y > min(spring.links*spring.linkLen, 2*spring.len)) {
+                ball.pos.y = spring.pos.y + min(spring.links*spring.linkLen, 2*spring.len);
+            } else {
+                ball.pos.y = mouseY;
+            }
+            ball.vel.set(0, 0);
+        }
+    }
     
-    spring.update();
-    spring.hook();
-    ball.apply(gravity);
-    ball.air();
-    ball.update();
+    if (running) {
+       //for (let i = 0; i < steps; i++) {
+            spring.update();
+            spring.hook();
+            ball.apply(gravity);
+            ball.air();
+            ball.update();
+        //}
+    }
     spring.show();
     ball.show();
 }
 
-P.mousePressed = function() {
-    if (mouseY > 0 && mouseX > 0 && mouseX < width && mouseY < height && running) {
-        ball.pos.set(mouseX, mouseY);
-        ball.vel.set(0, 0);
-    }
-}
-
 function reset() {
-    loop();
     runB.html('&#xf04c;');
     runB.elt.title = 'зупинити';
     running = true;
 
-    massS.value('1');
-    coeffS.value('0.025');
+    massS.value('1.25');
+    coeffS.value('0.125');
     ball = new Ball();
     spring = new Spring(ball);
 }
@@ -77,9 +83,7 @@ function run() {
         running = false;
         runB.html('&#xf04b;');
         runB.elt.title = 'продовжити';
-        noLoop();
     } else {
-        loop();
         runB.html('&#xf04c;');
         running = true;
         runB.elt.title = 'зупинити';
@@ -92,7 +96,7 @@ class Ball {
         this.radius = 10;
         this.pos = createVector(width*0.5, height*0.3 + 150);
         this.vel = createVector();
-        this.mass = 1;
+        this.mass = massS.value();
         this.acc = createVector();
     }
 
@@ -101,9 +105,15 @@ class Ball {
     }
 
     update() {
-        this.mass = mass;
+        this.mass = massS.value();
+        this.acc.mult(1/steps);
         this.vel.add(this.acc);
         this.pos.add(this.vel);
+        this.pos.x = width*0.5;
+        /*if (this.pos.y < spring.pos.y) {
+            this.pos.y += (spring.pos.y - this.pos.y);
+            this.vel.y = 0;
+        }*/
         this.acc.mult(0);
     }
 
@@ -121,12 +131,12 @@ class Ball {
 
 class Spring {
     constructor(ball) {
-        this.pos = createVector(width*0.5, height*0.3);
+        this.pos = createVector(width*0.5, height*0.1);
         this.ball = ball;
         this.len = 150;
         this.lenNow = 150;
-        this.coeff = 0.025;
-        this.links = 20;
+        this.coeff = coeffS.value();
+        this.links = 30;
         this.width = 8;
         this.linkLen = Math.hypot(this.len/this.links, 2*this.width);
     }
@@ -149,10 +159,13 @@ class Spring {
         vertex(this.lenNow, 0);
         endShape();
         pop();
+        /*if (this.lenNow > this.linkLen*this.links) {
+            console.log('(');
+        }*/
     }
 
     update() {
-        this.coeff = coeff;
+        this.coeff = coeffS.value();
     }
 
     hook() {

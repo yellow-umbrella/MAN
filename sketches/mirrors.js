e6033d06e3b91151b4d1d7f231000edd83e5debc
Dasha Niemkevych <daria.niemkevych@gmail.com>
2019-08-19 19:07:03 +0300

    Добавлен луч в зеркалах

--- sketches/mirrors.js
+++ sketches/mirrors.js
@@ -1,8 +1,8 @@
 //  плоскі дзеркала
 module.exports = new p5((P) => { with (P) {
 
-    let mirrors = [], description;
-    let running = true, confirmed = false;
+    let mirrors = [], rays = [], description;
+    let running = true, confirmed = false, EPS = 1E-8;
     let resetB, runB;
     
     P.setup = function() {
@@ -26,7 +26,15 @@ module.exports = new p5((P) => { with (P) {
         if (mouseIsPressed && mouseY > 0 && mouseX > 0 && mouseX < width && mouseY < height) {
             if (!confirmed) {
                 mirrors[mirrors.length - 1].pos2.set(mouseX, mouseY);
+            } else {
+                if (!rays[rays.length - 1].finished) {
+                    rays[rays.length - 1].change(mouseX, mouseY);
+                }
             }
+        }
+
+        for (let ray of rays) {
+            ray.show();
         }
         
         for (let mirror of mirrors) {
@@ -35,12 +43,13 @@ module.exports = new p5((P) => { with (P) {
     }
     
     function reset() {
-        loop();
+        //loop();
         runB.html('&#xf04c;');
         runB.elt.title = 'зупинити';
         running = true;
-        confirm = false;
+        confirmed = false;
         mirrors = [];
+        rays = [];
     }
     
     function run() {
@@ -48,9 +57,9 @@ module.exports = new p5((P) => { with (P) {
             running = false;
             runB.html('&#xf04b;');
             runB.elt.title = 'продовжити';
-            noLoop();
+            //noLoop();
         } else {
-            loop();
+            //loop();
             runB.html('&#xf04c;');
             running = true;
             runB.elt.title = 'зупинити';
@@ -61,8 +70,14 @@ module.exports = new p5((P) => { with (P) {
         if (mouseY > 0 && mouseX > 0 && mouseX < width && mouseY < height) {
             if (!confirmed) {
                 mirrors.push(new Mirror(mouseX, mouseY, mirrors.length));
+            } else {
+                rays.push(new Ray(mouseX, mouseY))
             }
         }
+    }
+
+    P.keyPressed = function() {
+        confirmed = true;
     }
     
     class Mirror {
@@ -76,6 +91,74 @@ module.exports = new p5((P) => { with (P) {
             stroke(255);
             strokeWeight(3);
             line(this.pos1.x, this.pos1.y, this.pos2.x, this.pos2.y);
+        }
+    }
+
+    class Ray {
+        constructor(x, y) {
+            this.dots = [];
+            this.dots.push(createVector(x, y));
+            this.finished = false;
+        }
+
+        show() {
+            let alfa = 100;
+            strokeWeight(4);
+            let prevDot = this.dots[0].copy();
+            for (let dot of this.dots) {
+                stroke(255, 255, 0, alfa);
+                line(prevDot.x, prevDot.y, dot.x, dot.y);
+                prevDot = dot.copy();
+                alfa -= 10;
+            }
+        }
+
+        change(x, y) {
+            let start = this.dots[0].copy();
+            this.dots = [];
+            this.dots.push(start.copy());
+            let tdot = createVector(x, y);
+            while (this.dots.length < 10) {
+                let A = tdot.y - start.y, B = start.x - tdot.x, C = tdot.x*start.y - start.x*tdot.y;
+                let check = false, ind = -1, dist = max(height, width), dot = createVector();
+                for (let mirror of mirrors) {
+                    let A1 = mirror.pos1.y - mirror.pos2.y;
+                    let B1 = mirror.pos2.x - mirror.pos1.x;
+                    let C1 = mirror.pos1.x*mirror.pos2.y - mirror.pos2.x*mirror.pos1.y;
+                    if (abs(A*B1 - A1*B) < EPS) {
+                        continue;
+                    }
+                    let tx = (B*C1 - B1*C)/(A*B1 - A1*B), ty = (A1*C - A*C1)/(A*B1 - A1*B);
+                    let t = createVector(tx, ty);
+                    let t1 = p5.Vector.sub(t, start).x/p5.Vector.sub(tdot, start).x;
+                    let t2 = p5.Vector.sub(t, mirror.pos1).x/p5.Vector.sub(mirror.pos2, mirror.pos1).x;
+                    if (t1 >= 0 && t2 >= 0 && t2 <= 1) {
+                        check = true;
+                        if (dist > p5.Vector.sub(start, t).mag()) {
+                            ind = mirror.ind, dist = p5.Vector.sub(start, t).mag(), dot = t.copy();
+                        }
+                    }
+                }
+                
+                if (check) {
+                    this.dots.push(dot);
+                    let angleBeg = (p5.Vector.sub(start,dot).dot(mirrors[ind].pos1, dot) > 0 ? mirrors[ind].pos1 : mirrors[ind].pos2);
+                    let vec = p5.Vector.sub(start, dot), vec1 = p5.Vector.sub(angleBeg, dot); 
+                    let angle = vec.angleBetween(vec1);
+                    angle = -2*angle + PI;
+                    if (vec.x*vec1.y - vec.y*vec1.x > 0) {
+                        angle *= -1;
+                    }
+                    vec.rotate(angle);
+                    tdot = p5.Vector.add(dot, vec);
+                    start = dot.copy();
+                } else {
+                    let vec = p5.Vector.sub(tdot, start);
+                    vec.setMag(2*sqrt(width*width + height*height));
+                    this.dots.push(p5.Vector.add(start, vec));
+                    break;
+                }
+            }
         }
     }
     }}, 'main');

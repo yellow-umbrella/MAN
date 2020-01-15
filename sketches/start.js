613e0f04e2f206a3e46dbe40c6b1080825288d12
Lyova Potyomkin <iamsuperman145@gmail.com>
2020-01-15 09:15:20 +0200

    Added the title back

--- sketches/start.js
+++ sketches/start.js
@@ -1,31 +1,27 @@
 module.exports = new p5((P) => { with (P) {
 
-let infoB, author, title;
-
 P.setup = function() {
     let canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(-200, -50);
     noStroke();
     fill('#0d261a');
-
-    infoB = createInfoB(P, 'start');
-    author = createDiv("&copy; Нємкевич Дар'я");
-    author.id('author');
-    title = createDiv('<center><h1>Фізичний Помічник</h1><h3>Експериментуй та розумій. Віртуальна фізична лабораторія.</h3><center/>');
-    title.style('font-size', '1.4em');
-    title.position(width/2, 0.1*height);
-    title.center('horizontal');
-    title.style('color','#0d261a');
-
-    loadFont('./fonts/Roundedmplus1c.ttf', font => textFont(font));
-    /*textAlign(CENTER, CENTER);
-    textSize('1.4em');*/
+    createInfoB(P, 'start');
+    let titleDiv = createDiv(` 
+        <center>
+            <h1>Фізичний Помічник</h1>
+            <h3>Експериментуй та розумій. Віртуальна фізична лабораторія.</h3>
+        </center>
+        <br>
+        <p id='description'>В даній програмі представлена добірка фізичних дослідів на різні теми з шкільної програми, 
+            які наочно продемонструють фізичні закони, а теоретична довідка допоможе краще 
+            засвоїти формули. Для початку роботи оберіть потрібний дослід в лівому меню.
+        </p>
+        <p id='author'>&copy; Нємкевич Дар'я</p>`
+    );
+    titleDiv.id('init');
+    select('#main').child('init');
+    noLoop();
 }
-
-P.draw = function() {
-    background('#26734d');
-}
-    
 }}, 'main');
         
         
